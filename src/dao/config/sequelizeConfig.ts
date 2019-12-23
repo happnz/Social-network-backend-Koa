0ddec2c6eb04c6b087ef75cd40f264f73a2f5427
Евгений Петрович <evgen280998@gmail.com>
2019-12-23 16:37:02 +0600

    fix wrong time conversion in pg

--- src/dao/config/sequelizeConfig.ts
+++ src/dao/config/sequelizeConfig.ts
@@ -2,6 +2,11 @@ import {Sequelize} from "sequelize";
 import * as config from "config";
 
 const dbConfig = config.get('db');
+
+require('pg').types.setTypeParser(1114, stringValue => { // prevent converting dates when reading from db
+    return new Date(stringValue + '+0000');
+});
+
 const sequelize: Sequelize = new Sequelize(dbConfig.dbName, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: "postgres",
